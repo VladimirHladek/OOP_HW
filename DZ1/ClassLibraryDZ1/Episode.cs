d20401953b1aee9ddb1024f6a78e8279e9e3976b
VladimirHladek <vladimir.hladek@student.ferit.hr>
2020-10-30 14:25:58 +0100

    Added DZ1

---
+++ DZ1/ClassLibraryDZ1/Episode.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DZ1
+{
+    public class Episode
+    {
+        public Episode (int viewerCount, double collectiveScore, double maxScore)
+        {
+            ViewerCount = viewerCount;
+            CollectiveScore = collectiveScore;
+            MaxScore = maxScore;
+        }
+        
+        public Episode()
+        {
+
+        }
+
+        private int ViewerCount { get; set; }
+        private double CollectiveScore { get; set; }
+        private double MaxScore { get; set; }
+
+        public void AddView(double score)
+        {
+            this.CollectiveScore += score;
+            
+            if (score > this.MaxScore)
+            {
+                this.MaxScore = score;
+            }
+
+            this.ViewerCount++;
+        }
+
+        public double GetMaxScore()
+        {
+            return this.MaxScore;
+        }
+
+        public int GetViewerCount()
+        {
+            return this.ViewerCount;
+        }
+
+        public double GetAverageScore()
+        {
+            return this.CollectiveScore / this.ViewerCount;
+        }
+    }
+        
+
+}
